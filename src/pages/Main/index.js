5d9c9bcfdc76d232e0ce6dae7d930042aa4ef346
Diego Rodrigues <diegurm@hotmail.com>
2020-01-16 21:55:28 -0300

    feat - Armazenado lista de repositorio no localstorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   constructor(props) {
@@ -13,6 +14,22 @@ export default class Main extends Component {
       repositories: [],
       loading: false,
     };
+  }
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+    if (repositories) {
+      this.setState({
+        repositories: JSON.parse(repositories),
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
   }
 
   handleInputChange = e => {
@@ -39,7 +56,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, repositories, loading } = this.state;
     return (
       <Container>
         <h1>
@@ -61,6 +78,17 @@ export default class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -43,9 +43,9 @@ const rotate = keyframes`
   }
 `;
 
-export const SubmitButton = styled.button.attrs(({ loading }) => ({
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-  disabled: loading,
+  disabled: props.loading,
 }))`
   background: #7159c1;
   border: 0;
@@ -69,4 +69,26 @@ export const SubmitButton = styled.button.attrs(({ loading }) => ({
         animation: ${rotate} 2s linear infinite;
       }
     `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
 `;
